485b6eed511dd7945834182d9f90d6b70f4cccdd
Guilherme Staffen <guilhermestaffen@gmail.com>
2025-05-26 22:09:41 -0300

    criando novo fluxo de reserva

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from reservas import views 
+from reservas import views
 
 urlpatterns = [
     path('', views.reservas, name='reservas'),
-    path('cadastro', views.cadastrar_reserva, name='cadastrar_reserva'),
-    path('cancelar/<int:reserva_id>', views.cancelar_reserva, name='cancelar_reserva'),
+    path('cadastro/reserva_inicial', views.reserva_inicial, name='reserva_inicial'),
+    path('cadastro/selecionar-quarto/', views.selecionar_quarto, name='selecionar_quarto'),
+    path('cadastro/resumo/', views.resumo_reserva, name='resumo_reserva'),
+    path('cadastro/limpar/', views.limpar_sessao_reserva, name='limpar_sessao_reserva'),
+    path('cancelar/<int:reserva_id>/', views.cancelar_reserva, name='cancelar_reserva'),
 ]
